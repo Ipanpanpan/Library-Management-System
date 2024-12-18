8faa7be1b128a2efe1a966da074fd303f70c21f8
Kevin Hendrawan <abelnathan8@gmail.com>
2024-12-18 15:40:39 +0800

    admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -6,7 +6,6 @@
 #include "Library.h" // Include the header file
 #include "Admin.h"
 
-Library adminobj;
 void Admin::addbook(ID, title, author, category, availability){
     adminobj.add_books(ID,title,author,category,availability);
 }
@@ -19,4 +18,23 @@ void Admin::deletebook(ID){
     adminobj.remove_book(ID);
 }
 
+void Admin::get_all_book(sortedby = NULL){
+    if (sorteddby = 1){
+        //id (Merge Sort)
+    }
+    else if (sortedby = 2){
+        //title (Bubble Sort)
+    }
+    else{
+        // print all (function from book import)
+    }
+}
+
+void Admin::get_all_transaction(){
+    // selection sort
+}
+
+void Admin::search_transaction_receipt(user_id){
+    // binary search
+}
 
